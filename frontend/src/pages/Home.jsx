5eb9dba7518c33b1e54afdc7bc0169d0ef0d9848
Marko Janjic <marko.m.janjic@gmail.com>
2024-06-17 22:22:07 +0200

    changed data fetching on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,23 @@
-import { useState, useEffect } from "react";
 import CountCard from "../components/CountCard";
 import LoadingScreen from "../components/LoadingScreen";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { data, isLoading } = useFetch("http://localhost:3000/");
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/", { mode: "cors" });
-        const someData = await res.json();
-        setData(someData.data);
-        setLoading(false);
-        console.log(someData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getData();
-  }, []);
+  if (isLoading) return <LoadingScreen />;
 
   return (
     <div className="relative">
-      {loading && <LoadingScreen />}
-      {data && (
+      {data.data && (
         <section className="flex flex-col flex-1 ">
           <div className="flex items-center justify-center p-5 w-4/5 mx-auto">
             <h1 className="text-4xl font-bold">Bookstore</h1>
           </div>
           <div className="grid grid-cols-3 flex-1 w-4/5 mx-auto px-5 gap-2 pt-10">
-            <CountCard data={data.booksCount} label={"Books"} />
-            <CountCard data={data.authorsCount} label={"Authors"} />
-            <CountCard data={data.genreCount} label={"Genres"} />
+            <CountCard data={data.data.booksCount} label={"Books"} />
+            <CountCard data={data.data.authorsCount} label={"Authors"} />
+            <CountCard data={data.data.genreCount} label={"Genres"} />
           </div>
         </section>
       )}
